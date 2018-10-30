78bef9d1fdb0f9d52d76c6ffec843648b3977e7e
Gabe <gabe4k@gmail.com>
2018-10-30 16:58:47 -0700

    Fixed trailing decimal on whole numbers

--- eval/src/Eval.hs
+++ eval/src/Eval.hs
@@ -13,7 +13,15 @@ eval' :: String -> String
 eval' input =
   case parseExp input of
     Left  msg -> msg
-    Right e   -> show (simplify e)
+    Right e   ->
+      let r = simplify e
+          output = show r
+      in
+        if r == fromIntegral (truncate r)
+          then take (length output - 2) output
+          else show r
+
+
 
 
 foreign export ccall eval :: CString -> IO CString
--- setup.bat
+++ setup.bat
@@ -1,10 +1,7 @@
-@echo off
-
 cd eval
-cabal update
 cabal sandbox init
 cabal install --dependencies-only
 cabal build
 cd ..
 
-copy "%cd%\eval\dist\build\eval\eval.dll" "%cd%"
+copy "%cd%\eval\dist\build\eval\eval.dll" "%cd%\Calc\"
